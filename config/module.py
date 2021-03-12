0f34884f4ae8eb03691b171eb5cc008d0e5dffd4
George Paul <georgerahul.paul@microchip.com>
2021-03-12 10:37:15 +0530

    Changed description from '10BaseT1S driver' to '10BASE-T1S PHY' .[MH3-51486]

--- config/module.py
+++ config/module.py
@@ -1,6 +1,6 @@
-######################  Harmony 10BaseT1S driver  ######################
+######################  Harmony 10BASE-T1S PHY  ######################
 def loadModule():
-    print("Load Module: Harmony 10BaseT1S driver")
+    print("Load Module: Harmony 10BASE-T1S PHY")
 
     #Driver for MCHP LAN867x PHY
     drvExtPhyLan867xComponent = Module.CreateComponent("drvExtPhyLan867x", "LAN867x", "/Harmony/Drivers/PHY Driver", "driver/lan867x/config/drv_extphy_lan867x.py")

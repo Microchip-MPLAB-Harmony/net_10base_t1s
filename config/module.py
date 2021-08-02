c10c5e6503de348de317397eb757dbcfcdf89865
Jan Huber <jan.huber@microchip.com>
2021-08-02 09:49:28 +0200

    Make net_10base_t1s repo visible to net v3.7.3 [MH59139]

--- config/module.py
+++ config/module.py
@@ -1,8 +1,9 @@
-######################  Harmony 10BASE-T1S PHY  ######################
+######################  Harmony 3 10BASE-T1S PHY  ######################
 def loadModule():
-    print("Load Module: Harmony 10BASE-T1S PHY")
+    print("Load Module: Harmony 3 10BASE-T1S PHY")
 
-    #Driver for MCHP LAN867x PHY
+    # Driver for MCHP LAN867x PHY
     drvExtPhyLan867xComponent = Module.CreateComponent("drvExtPhyLan867x", "LAN867x", "/Harmony/Drivers/PHY Driver", "driver/lan867x/config/drv_extphy_lan867x.py")
-    drvExtPhyLan867xComponent.addCapability("libdrvExtPhyLan867x", "PHY", True)   
+    drvExtPhyLan867xComponent.addCapability("libdrvExtPhyLan867x", "PHY", False)
     drvExtPhyLan867xComponent.addDependency("LAN867x_MIIM_Dependency", "MIIM", None, True, True)
+    drvExtPhyLan867xComponent.setDisplayType("PHY Layer")

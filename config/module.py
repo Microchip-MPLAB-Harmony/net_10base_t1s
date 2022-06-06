475ccccdfe4248dcdc004b068ad317886a9810d0
Adhithyan K G <adhithyan.kg@microchip.com>
2022-06-06 12:00:57 +0530

    Remove Harmony from driver path [MH3-69118]

--- config/module.py
+++ config/module.py
@@ -3,7 +3,7 @@ def loadModule():
     print("Load Module: Harmony 3 10BASE-T1S PHY")
 
     # Driver for MCHP LAN867x PHY
-    drvExtPhyLan867xComponent = Module.CreateComponent("drvExtPhyLan867x", "LAN867x", "/Harmony/Drivers/PHY Driver", "driver/lan867x/config/drv_extphy_lan867x.py")
+    drvExtPhyLan867xComponent = Module.CreateComponent("drvExtPhyLan867x", "LAN867x", "/Drivers/PHY Driver", "driver/lan867x/config/drv_extphy_lan867x.py")
     drvExtPhyLan867xComponent.addCapability("libdrvExtPhyLan867x", "PHY", False)
     drvExtPhyLan867xComponent.addDependency("LAN867x_MIIM_Dependency", "MIIM", None, True, True)
     drvExtPhyLan867xComponent.setDisplayType("PHY Layer")

c1de8080efceccb82fe0808cdc8590ecb678d114
Jan Huber <jan.huber@microchip.com>
2023-02-10 09:35:17 +0100

    Verify and update copyright license header for release v1.2.0 [MH3-62201]

--- driver/lan865x/src/dynamic/tc6/tc6-queue.h
+++ driver/lan865x/src/dynamic/tc6/tc6-queue.h
@@ -1,35 +1,34 @@
+//DOM-IGNORE-BEGIN
+/*
+Copyright (C) 2023, Microchip Technology Inc., and its subsidiaries. All rights reserved.
+
+The software and documentation is provided by microchip and its contributors
+"as is" and any express, implied or statutory warranties, including, but not
+limited to, the implied warranties of merchantability, fitness for a particular
+purpose and non-infringement of third party intellectual property rights are
+disclaimed to the fullest extent permitted by law. In no event shall microchip
+or its contributors be liable for any direct, indirect, incidental, special,
+exemplary, or consequential damages (including, but not limited to, procurement
+of substitute goods or services; loss of use, data, or profits; or business
+interruption) however caused and on any theory of liability, whether in contract,
+strict liability, or tort (including negligence or otherwise) arising in any way
+out of the use of the software and documentation, even if advised of the
+possibility of such damage.
+
+Except as expressly permitted hereunder and subject to the applicable license terms
+for any third-party software incorporated in the software and any applicable open
+source software license terms, no license or other rights, whether express or
+implied, are granted under any patent or other intellectual property rights of
+Microchip or any third party.
+*/
+//DOM-IGNORE-END
+
 #ifndef TC6_QUEUE_
 #define TC6_QUEUE_
 
 /* DO NOT MODIFY: generated from tc6-queue.conf */
 /*------------------------------------------------------------------------------------------------*/
 /* Low Level Driver for OpenAlliance TC6 10BASE-T1S MACPHY via SPI protocol                       */
-/* Copyright 2021, Microchip Technology Inc. and its subsidiaries.                                */
-/*                                                                                                */
-/* Redistribution and use in source and binary forms, with or without                             */
-/* modification, are permitted provided that the following conditions are met:                    */
-/*                                                                                                */
-/* 1. Redistributions of source code must retain the above copyright notice, this                 */
-/*    list of conditions and the following disclaimer.                                            */
-/*                                                                                                */
-/* 2. Redistributions in binary form must reproduce the above copyright notice,                   */
-/*    this list of conditions and the following disclaimer in the documentation                   */
-/*    and/or other materials provided with the distribution.                                      */
-/*                                                                                                */
-/* 3. Neither the name of the copyright holder nor the names of its                               */
-/*    contributors may be used to endorse or promote products derived from                        */
-/*    this software without specific prior written permission.                                    */
-/*                                                                                                */
-/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"                    */
-/* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE                      */
-/* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE                 */
-/* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE                   */
-/* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL                     */
-/* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR                     */
-/* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER                     */
-/* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,                  */
-/* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE                  */
-/* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                           */
 /*------------------------------------------------------------------------------------------------*/
 
 #include <stdint.h>
